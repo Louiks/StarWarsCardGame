6814f14baa60406a904461eb4cfd2e4b46cb30af
Louiks <kokot.jakub@wp.pl>
2025-04-01 12:52:42 +0200

    Prettied code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,22 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TranslateModule } from "@ngx-translate/core";
-import { AboutPageComponent } from "./about/about-page/about-page.component";
+import { AboutPageComponent } from "./about-page/about-page.component";
 import { SharedModule } from "../shared/shared.module";
-import { HowToPageComponent } from "./how-to/how-to-page/how-to-page.component";
-
+import { HowToPageComponent } from "./how-to-page/how-to-page.component";
 
 @NgModule({
-  declarations: [
-    HomePageComponent,
-    AboutPageComponent,
-    HowToPageComponent
-  ],
-  imports: [
-    CommonModule,
-    TranslateModule,
-    SharedModule
-  ]
+    declarations: [
+        HomePageComponent,
+        AboutPageComponent,
+        HowToPageComponent
+    ],
+    imports: [
+        CommonModule,
+        TranslateModule,
+        SharedModule
+    ]
 })
 export class HomeModule {
 }
